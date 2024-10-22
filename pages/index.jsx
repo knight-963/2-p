17e92ee6a34636b34491337142d35b83bcac7132
Hikmatilla <Hikmatilla@gmail.com>
2024-10-22 15:50:39 +0500

    [hi]

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,39 @@
+import Link from 'next/link';
+import styles from '../styles/nsvbar.module.css';
+
 
 export default function Home() {
   return (
     <div>
-      <h1 className="flex justify-center text-9xl text-red-500">salom</h1>
+  <header className={styles.navbar}>
+      <div className={styles.logo}>
+        <h1>SmartCity</h1>
+      </div>
+      <nav className={styles.navLinks}>
+        <a href="#home">
+          <a>Home</a>
+        </a>
+        <a href="#services">
+          <a>Services</a>
+        </a>
+        <a href="#news">
+          <a>News</a>
+        </a>
+        <a href="#projects">
+          <a>Projects</a>
+        </a>
+        <a href="#contacts">
+          <a>Contacts</a>
+        </a>
+      </nav>
+      <div className={styles.burger}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </header> 
+
+
     </div>
   );
 }
